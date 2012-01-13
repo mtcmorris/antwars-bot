667006a7f7eac900883c7ba839873ea0789d3831
Michael Morris <mtcmorris@gmail.com>
2012-01-13 17:03:54 +1100

    Initial Commit

---
+++ play_one_game.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+./playgame.py --player_seed 42 --end_wait=0.25 --nolaunch --log_stdout --log_dir game_logs --turns 1000 --map_file maps/maze/maze_04p_01.map "$@" "python sample_bots/python/HunterBot.py" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "python sample_bots/python/GreedyBot.py"
+
