df10a227184bec421d489db4ba3ca28c26c7f9df
Michael Morris <mtcmorris@gmail.com>
2012-01-14 16:29:04 +1100

    Moving to food

--- ruby_starter_package/lib/bot.rb
+++ ruby_starter_package/lib/bot.rb
@@ -20,15 +20,65 @@ class Bot
   def run(ai)
     # your turn code here
     @logger.log "Ran turn"
+    # Get food locations
+    food = food_locations(ai)
+    @logger.log food.inspect
   	ai.my_ants.each do |ant|
-  		# try to go north, if possible; otherwise try east, south, west.
+  	  food_to_move_to = food.sort{|a, b| distance(b, [ant.row, ant.col]) <=> distance(a, [ant.row, ant.col])}.pop
+  	  if food_to_move_to
+    	  vector = get_vector(food_to_move_to, [ant.row, ant.col])
+    	  vector.each{|dir|
+          if ant.square.neighbor(dir).land? && !ant.square.neighbor(dir).ant?
+            ant.order dir
+            break
+          end
+    	  }
+  	  end
 
-  		[:N, :E, :S, :W].each do |dir|
-  			if ant.square.neighbor(dir).land?
-  				ant.order dir
-  				break
-  			end
+      if !ant.moved?
+    		# Move randomly
+
+    		[:N, :E, :S, :W].shuffle do |dir|
+    			if ant.square.neighbor(dir).land? && !ant.square.neighbor(dir).ant?
+    				ant.order dir
+    				break
+    			end
+    		end
   		end
   	end
+  end
+
+  def distance(coord1, coord2)
+    (coord1[0] - coord2[0]).abs
+    +
+    (coord1[1] - coord2[1]).abs
+  end
+
+  def get_vector(coord1, coord2)
+    vector = []
+    if coord1[0] < coord2[0]
+      vector.push :N
+    elsif coord1[0] > coord2[0]
+      vector.push :S
+    end
+
+    if coord1[1] < coord2[1]
+      vector.push :W
+    elsif coord1[0] > coord2[0]
+      vector.push :E
+    end
+    vector
+  end
+
+  def food_locations(ai)
+    food_coords = []
+    ai.map.each { |r|
+      r.each{|c|
+        if c.food
+          food_coords.push [c.row, c.col]
+        end
+      }
+    }
+    food_coords
   end
 end
