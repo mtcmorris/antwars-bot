5dc813503daaf48d09a2917aff7eed82f10bd39c
Michael Morris <mtcmorris@gmail.com>
2012-01-14 13:15:50 +1100

    Updated play one game for more correctness

--- play_one_game.sh
+++ play_one_game.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-./playgame.py -So --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/maze/maze_04p_01.map "$@" "ruby ruby_starter_package/bot.rb" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "python sample_bots/python/GreedyBot.py" | java -jar visualizer.jar
+./playgame.py -So --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/maze/maze_04p_01.map "$@" "ruby ruby_starter_package/lib/bot.rb" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "python sample_bots/python/GreedyBot.py" | java -jar visualizer.jar
 
