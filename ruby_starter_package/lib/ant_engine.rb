a48c46b2c04a7fd9147b75dc78706c844a7feeba
Pat Allan <pat@freelancing-gods.com>
2012-01-14 13:07:55 +1100

    Organising Code!

---
+++ ruby_starter_package/lib/ant_engine.rb
@@ -0,0 +1,11 @@
+# Ants AI Challenge framework
+# by Matma Rex (matma.rex@gmail.com)
+# Released under CC-BY 3.0 license
+
+module AntEngine
+  #
+end
+
+require 'ant_engine/ai'
+require 'ant_engine/ant'
+require 'ant_engine/square'
--- ruby_starter_package/lib/bot.rb
+++ ruby_starter_package/lib/bot.rb
@@ -1,23 +1,31 @@
-require 'ants'
+require 'ant_engine'
 
 class Bot
-  def self.run(ai = AI.new)
+  def self.run(ai = AntEngine::AI.new)
+    bot = new
     ai.setup do |ai|
-    	# your setup code here, if any
+    	bot.setup ai
     end
 
     ai.run do |ai|
-    	# your turn code here
-
-    	ai.my_ants.each do |ant|
-    		# try to go north, if possible; otherwise try east, south, west.
-    		[:N, :E, :S, :W].each do |dir|
-    			if ant.square.neighbor(dir).land?
-    				ant.order dir
-    				break
-    			end
-    		end
-    	end
+    	bot.run ai
     end
+  end
+
+  def setup(ai)
+    #
+  end
+
+  def run(ai)
+    # your turn code here
+  	ai.my_ants.each do |ant|
+  		# try to go north, if possible; otherwise try east, south, west.
+  		[:N, :E, :S, :W].each do |dir|
+  			if ant.square.neighbor(dir).land?
+  				ant.order dir
+  				break
+  			end
+  		end
+  	end
   end
 end
